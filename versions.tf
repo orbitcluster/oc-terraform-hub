489e89236f7a470a4c3e1afa2470f3103de9e9e2
maav-sk <shubhankarkhanda@gmail.com>
2026-01-05 20:13:33 +0530

    fix: add helm/k8s providers

--- versions.tf
+++ versions.tf
@@ -34,7 +34,7 @@ provider "helm" {
     host                   = module.eks_setup.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks_setup.cluster_certificate_authority_data)
 
-    exec {
+    exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
       args        = ["eks", "get-token", "--cluster-name", module.eks_setup.cluster_name]
